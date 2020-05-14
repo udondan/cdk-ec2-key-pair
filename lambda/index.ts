c204b47f3e608df19ce8ad26da2caa63235eae55
Daniel Schroeder <udondan@users.noreply.github.com>
2020-05-14 16:26:14 +0200

    Fix response values (#9)
    * make it prettier
    
    * fixes response values
    
    * version bump
    
    * Updates version references
    
    * update dep to latest version
    
    Co-authored-by: GitHub Action <action@github.com>

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,4 +1,4 @@
-import { CustomResource, Event, StandardLogger } from 'aws-cloudformation-custom-resource';
+import { CustomResource, Event, LambdaEvent, StandardLogger } from 'aws-cloudformation-custom-resource';
 import { Callback, Context } from 'aws-lambda';
 import AWS = require('aws-sdk');
 
@@ -6,7 +6,11 @@ const ec2 = new AWS.EC2();
 const secretsmanager = new AWS.SecretsManager();
 const logger = new StandardLogger();
 
-export const handler = function (event: Event = {}, context: Context, callback: Callback) {
+export const handler = function (
+    event: LambdaEvent,
+    context: Context,
+    callback: Callback
+) {
     new CustomResource(event, context, callback, logger)
         .onCreate(Create)
         .onUpdate(Update)
@@ -15,7 +19,9 @@ export const handler = function (event: Event = {}, context: Context, callback: 
 };
 
 function Create(event: Event): Promise<Event | AWS.AWSError> {
-    logger.info(`Attempting to create EC2 Key Pair ${event.ResourceProperties.Name}`);
+    logger.info(
+        `Attempting to create EC2 Key Pair ${event.ResourceProperties.Name}`
+    );
     return new Promise(function (resolve, reject) {
         createKeyPair(event)
             .then(savePrivaterKey)
@@ -29,10 +35,18 @@ function Create(event: Event): Promise<Event | AWS.AWSError> {
 }
 
 function Update(event: Event): Promise<Event | AWS.AWSError> {
-    logger.info(`Attempting to update EC2 Key Pair ${event.OldResourceProperties.Name}`);
+    logger.info(
+        `Attempting to update EC2 Key Pair ${event.OldResourceProperties.Name}`
+    );
     return new Promise(function (resolve, reject) {
-        if (event.ResourceProperties.Name !== event.OldResourceProperties.Name) {
-            reject(new Error('A Key Pair cannot be renamed. Please create a new Key Pair instead'));
+        if (
+            event.ResourceProperties.Name !== event.OldResourceProperties.Name
+        ) {
+            reject(
+                new Error(
+                    'A Key Pair cannot be renamed. Please create a new Key Pair instead'
+                )
+            );
         }
 
         updateKeyPair(event)
@@ -49,7 +63,9 @@ function Update(event: Event): Promise<Event | AWS.AWSError> {
 }
 
 function Delete(event: any): Promise<Event | AWS.AWSError> {
-    logger.info(`Attempting to delete EC2 Key Pair ${event.ResourceProperties.Name}`);
+    logger.info(
+        `Attempting to delete EC2 Key Pair ${event.ResourceProperties.Name}`
+    );
     return new Promise(function (resolve, reject) {
         deleteKeyPair(event)
             .then(deletePrivaterKey)
@@ -64,108 +80,148 @@ function Delete(event: any): Promise<Event | AWS.AWSError> {
 
 function createKeyPair(event: Event): Promise<Event> {
     return new Promise(function (resolve, reject) {
-        ec2.createKeyPair({
-            KeyName: event.ResourceProperties.Name,
-        }, function (err: AWS.AWSError, data: AWS.EC2.KeyPair) {
-            if (err) return reject(err);
-            event.KeyName = data.KeyName;
-            event.KeyFingerprint = data.KeyFingerprint;
-            event.KeyMaterial = data.KeyMaterial;
-            resolve(event);
-        });
+        ec2.createKeyPair(
+            {
+                KeyName: event.ResourceProperties.Name,
+            },
+            function (err: AWS.AWSError, data: AWS.EC2.KeyPair) {
+                if (err) return reject(err);
+                event.addResponseValue('KeyPairName', data.KeyName);
+                event.KeyFingerprint = data.KeyFingerprint;
+                event.KeyMaterial = data.KeyMaterial;
+                resolve(event);
+            }
+        );
     });
 }
 
 function updateKeyPair(event: Event): Promise<Event> {
     return new Promise(function (resolve, reject) {
         // there is nothing to update. a key cannot be changed
-        event.KeyName = event.ResourceProperties.Name;
+        event.addResponseValue('KeyPairName', event.ResourceProperties.Name);
         resolve(event);
     });
 }
 
 function deleteKeyPair(event: Event): Promise<Event> {
     return new Promise(function (resolve, reject) {
-        ec2.deleteKeyPair({
-            KeyName: event.ResourceProperties.Name,
-        }, function (err: AWS.AWSError, data: {}) {
-            if (err) return reject(err);
-            event.KeyName = event.ResourceProperties.Name;
-            resolve(event);
-        });
+        ec2.deleteKeyPair(
+            {
+                KeyName: event.ResourceProperties.Name,
+            },
+            function (err: AWS.AWSError, data: {}) {
+                if (err) return reject(err);
+                event.addResponseValue(
+                    'KeyPairName',
+                    event.ResourceProperties.Name
+                );
+                resolve(event);
+            }
+        );
     });
 }
 
 function savePrivaterKey(event: Event): Promise<Event> {
     return new Promise(function (resolve, reject) {
-        secretsmanager.createSecret({
-            Name: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
-            Description: event.ResourceProperties.Description,
-            SecretString: event.KeyMaterial,
-            KmsKeyId: event.ResourceProperties.Kms,
-            Tags: makeTags(event, event.ResourceProperties),
-        }, function (err: AWS.AWSError, data: AWS.SecretsManager.CreateSecretResponse) {
-            if (err) return reject(err);
-            event.secretARN = data.ARN;
-            resolve(event);
-        });
+        secretsmanager.createSecret(
+            {
+                Name: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
+                Description: event.ResourceProperties.Description,
+                SecretString: event.KeyMaterial,
+                KmsKeyId: event.ResourceProperties.Kms,
+                Tags: makeTags(event, event.ResourceProperties),
+            },
+            function (
+                err: AWS.AWSError,
+                data: AWS.SecretsManager.CreateSecretResponse
+            ) {
+                if (err) return reject(err);
+                event.addResponseValue('PrivateKeyARN', data.ARN);
+                resolve(event);
+            }
+        );
     });
 }
 
 function updatePrivaterKey(event: Event): Promise<Event> {
     return new Promise(function (resolve, reject) {
-        secretsmanager.updateSecret({
-            SecretId: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
-            Description: event.ResourceProperties.Description,
-            KmsKeyId: event.ResourceProperties.Kms,
-        }, function (err: AWS.AWSError, data: AWS.SecretsManager.UpdateSecretResponse) {
-            if (err) return reject(err);
-            event.secretARN = data.ARN;
-            resolve(event);
-        });
+        secretsmanager.updateSecret(
+            {
+                SecretId: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
+                Description: event.ResourceProperties.Description,
+                KmsKeyId: event.ResourceProperties.Kms,
+            },
+            function (
+                err: AWS.AWSError,
+                data: AWS.SecretsManager.UpdateSecretResponse
+            ) {
+                if (err) return reject(err);
+                event.addResponseValue('PrivateKeyARN', data.ARN);
+                resolve(event);
+            }
+        );
     });
 }
 
 function updatePrivaterKeyAddTags(event: Event): Promise<Event> {
-    logger.info(`Attempting to update tags for EC2 private key ${event.ResourceProperties.Name}`);
+    logger.info(
+        `Attempting to update tags for EC2 private key ${event.ResourceProperties.Name}`
+    );
     return new Promise(function (resolve, reject) {
         const oldTags = makeTags(event, event.OldResourceProperties);
         const newTags = makeTags(event, event.ResourceProperties);
         if (JSON.stringify(oldTags) == JSON.stringify(newTags)) {
-            logger.info(`No changes of tags detected for EC2 private key ${event.ResourceProperties.Name}. Not attempting any update`);
+            logger.info(
+                `No changes of tags detected for EC2 private key ${event.ResourceProperties.Name}. Not attempting any update`
+            );
             return resolve(event);
         }
 
-        secretsmanager.tagResource({
-            SecretId: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
-            Tags: newTags,
-        }, function (err: AWS.AWSError, data: {}) {
-            if (err) return reject(err);
-            resolve(event);
-        });
+        secretsmanager.tagResource(
+            {
+                SecretId: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
+                Tags: newTags,
+            },
+            function (err: AWS.AWSError, data: {}) {
+                if (err) return reject(err);
+                resolve(event);
+            }
+        );
     });
 }
 
 function updatePrivaterKeyRemoveTags(event: Event): Promise<Event> {
-    logger.info(`Attempting to remove some tags for EC2 private key ${event.ResourceProperties.Name}`);
+    logger.info(
+        `Attempting to remove some tags for EC2 private key ${event.ResourceProperties.Name}`
+    );
     return new Promise(function (resolve, reject) {
         const oldTags = makeTags(event, event.OldResourceProperties);
         const newTags = makeTags(event, event.ResourceProperties);
         const tagsToRemove = getMissingTags(oldTags, newTags);
-        if (JSON.stringify(oldTags) == JSON.stringify(newTags) || !tagsToRemove.length) {
-            logger.info(`No changes of tags detected for EC2 private key ${event.ResourceProperties.Name}. Not attempting any update`);
+        if (
+            JSON.stringify(oldTags) == JSON.stringify(newTags) ||
+            !tagsToRemove.length
+        ) {
+            logger.info(
+                `No changes of tags detected for EC2 private key ${event.ResourceProperties.Name}. Not attempting any update`
+            );
             return resolve(event);
         }
 
-        logger.info(`Will remove the following tags: ${JSON.stringify(tagsToRemove)}`);
-        secretsmanager.untagResource({
-            SecretId: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
-            TagKeys: tagsToRemove,
-        }, function (err: AWS.AWSError, data: {}) {
-            event.results.push({ data: data, error: err });
-            if (err) reject(err);
-            else resolve(event);
-        });
+        logger.info(
+            `Will remove the following tags: ${JSON.stringify(tagsToRemove)}`
+        );
+        secretsmanager.untagResource(
+            {
+                SecretId: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
+                TagKeys: tagsToRemove,
+            },
+            function (err: AWS.AWSError, data: {}) {
+                event.results.push({ data: data, error: err });
+                if (err) reject(err);
+                else resolve(event);
+            }
+        );
     });
 }
 
@@ -175,33 +231,45 @@ function deletePrivaterKey(event: Event): Promise<Event> {
             SecretId: `${event.ResourceProperties.SecretPrefix}${event.ResourceProperties.Name}`,
         };
 
-        const removePrivateKeyAfterDays = event.ResourceProperties.RemovePrivateKeyAfterDays as number;
+        const removePrivateKeyAfterDays = event.ResourceProperties
+            .RemovePrivateKeyAfterDays as number;
 
         if (removePrivateKeyAfterDays > 0) {
-            options.RecoveryWindowInDays = event.ResourceProperties.RemovePrivateKeyAfterDays;
+            options.RecoveryWindowInDays =
+                event.ResourceProperties.RemovePrivateKeyAfterDays;
         } else {
             options.ForceDeleteWithoutRecovery = true;
         }
 
-        secretsmanager.deleteSecret(options, function (err: AWS.AWSError, data: AWS.SecretsManager.DeleteSecretResponse) {
+        secretsmanager.deleteSecret(options, function (
+            err: AWS.AWSError,
+            data: AWS.SecretsManager.DeleteSecretResponse
+        ) {
             if (err) return reject(err);
-            event.secretARN = data.ARN;
+            event.addResponseValue('PrivateKeyARN', data.ARN);
             resolve(event);
         });
     });
 }
 
-function makeTags(event: Event, properties: any): AWS.SecretsManager.TagListType {
-    const tags: AWS.SecretsManager.TagListType = [{
-        Key: 'aws-cloudformation:stack-id',
-        Value: event.StackId,
-    }, {
-        Key: 'aws-cloudformation:stack-name',
-        Value: properties.StackName,
-    }, {
-        Key: 'aws-cloudformation:logical-id',
-        Value: event.LogicalResourceId,
-    }];
+function makeTags(
+    event: Event,
+    properties: any
+): AWS.SecretsManager.TagListType {
+    const tags: AWS.SecretsManager.TagListType = [
+        {
+            Key: 'aws-cloudformation:stack-id',
+            Value: event.StackId,
+        },
+        {
+            Key: 'aws-cloudformation:stack-name',
+            Value: properties.StackName,
+        },
+        {
+            Key: 'aws-cloudformation:logical-id',
+            Value: event.LogicalResourceId,
+        },
+    ];
     if ('Tags' in properties) {
         Object.keys(properties.Tags).forEach(function (key: string) {
             tags.push({
@@ -213,7 +281,10 @@ function makeTags(event: Event, properties: any): AWS.SecretsManager.TagListType
     return tags;
 }
 
-function getMissingTags(oldTags: AWS.SecretsManager.TagListType, newTags: AWS.SecretsManager.TagListType): string[] {
+function getMissingTags(
+    oldTags: AWS.SecretsManager.TagListType,
+    newTags: AWS.SecretsManager.TagListType
+): string[] {
     var missing = oldTags.filter(missingTags(newTags));
     return missing.map(function (tag: AWS.SecretsManager.Tag) {
         return tag.Key;
@@ -222,8 +293,10 @@ function getMissingTags(oldTags: AWS.SecretsManager.TagListType, newTags: AWS.Se
 
 function missingTags(newTags: AWS.SecretsManager.TagListType) {
     return (currentTag: AWS.SecretsManager.Tag) => {
-        return newTags.filter((newTag: any) => {
-            return newTag.Key == currentTag.Key;
-        }).length == 0;
+        return (
+            newTags.filter((newTag: any) => {
+                return newTag.Key == currentTag.Key;
+            }).length == 0
+        );
     };
 }
--- lib/index.ts
+++ lib/index.ts
@@ -16,100 +16,103 @@ export enum KeyLength {
 }
 
 /**
-* Definition of EC2 Key Pair
-*/
+ * Definition of EC2 Key Pair
+ */
 export interface KeyPairProps extends cdk.ResourceProps {
-
     /**
-    * Name of the Key Pair
-    *
-    * In AWS Secrets Manager the key will be prefixed with `ec2-private-key/`.
-    *
-    * The name can be up to 255 characters long. Valid characters include _, -, a-z, A-Z, and 0-9.
-    */
+     * Name of the Key Pair
+     *
+     * In AWS Secrets Manager the key will be prefixed with `ec2-private-key/`.
+     *
+     * The name can be up to 255 characters long. Valid characters include _, -, a-z, A-Z, and 0-9.
+     */
     readonly name: string;
 
     /**
-    * The description for the key in AWS Secrets Manager
-    * @default - ''
-    */
+     * The description for the key in AWS Secrets Manager
+     * @default - ''
+     */
     readonly description?: string;
 
     /**
-    * Number of bits in the key. Valid options are 2048 and 4096
-    *
-    * @default - 2048
-    */
+     * Number of bits in the key. Valid options are 2048 and 4096
+     *
+     * @default - 2048
+     */
     readonly keyLength?: KeyLength;
 
     /**
-    * The KMS key to use to encrypt the private key with
-    *
-    * This needs to be a key created in the same stack. You cannot use a key imported via ARN.
-    *
-    * @default - `alias/aws/secretsmanager`
-    */
+     * The KMS key to use to encrypt the private key with
+     *
+     * This needs to be a key created in the same stack. You cannot use a key imported via ARN.
+     *
+     * @default - `alias/aws/secretsmanager`
+     */
     readonly kms?: kms.Key;
 
     /**
-    * Tags that will be applied to the private key in the AWS Secrets Manager
-    *
-    * EC2 Key Pairs themselves don't support tags
-    *
-    * @default - `alias/aws/secretsmanager`
-    */
+     * Tags that will be applied to the private key in the AWS Secrets Manager
+     *
+     * EC2 Key Pairs themselves don't support tags
+     *
+     * @default - `alias/aws/secretsmanager`
+     */
     readonly tags?: {
         [key: string]: string;
     };
 
     /**
-    * When the resource is destroyed, after how many days the private key in the AWS Secrets Manager should be deleted.
-    *
-    * Valid values are 0 and 7 to 30
-    *
-    * @default 0
-    */
+     * When the resource is destroyed, after how many days the private key in the AWS Secrets Manager should be deleted.
+     *
+     * Valid values are 0 and 7 to 30
+     *
+     * @default 0
+     */
     readonly removePrivateKeyAfterDays?: number;
 
     /**
-    * Prefix for the secret in AWS Secrets Manager.
-    *
-    * @default `ec2-private-key/`
-    */
+     * Prefix for the secret in AWS Secrets Manager.
+     *
+     * @default `ec2-private-key/`
+     */
     readonly secretPrefix?: string;
 }
 
 /**
-* An EC2 Key Pair
-*/
+ * An EC2 Key Pair
+ */
 export class KeyPair extends cdk.Construct implements cdk.ITaggable {
-
     /**
-    * ARN of the private key in AWS Secrets Manager
-    */
+     * ARN of the private key in AWS Secrets Manager
+     */
     public readonly arn: string = '';
 
     /**
-    * Name of the Key Pair
-    */
+     * Name of the Key Pair
+     */
     public readonly name: string = '';
 
     /**
-    * Resource tags
-    */
+     * Resource tags
+     */
     public readonly tags: cdk.TagManager;
 
     /**
-    * Defines a new EC2 Key Pair. The private key will be stored in AWS Secrets Manager
-    */
+     * Defines a new EC2 Key Pair. The private key will be stored in AWS Secrets Manager
+     */
     constructor(scope: cdk.Construct, id: string, props: KeyPairProps) {
         super(scope, id);
 
-        if (props.removePrivateKeyAfterDays && (
-            props.removePrivateKeyAfterDays < 0 ||
-            props.removePrivateKeyAfterDays > 0 && props.removePrivateKeyAfterDays < 7 ||
-            props.removePrivateKeyAfterDays > 30)) {
-            scope.node.addError(`Parameter removePrivateKeyAfterDays must be 0 or between 7 and 30. Got ${props.removePrivateKeyAfterDays}`);
+        if (
+            props.removePrivateKeyAfterDays &&
+            (props.removePrivateKeyAfterDays < 0 ||
+                (props.removePrivateKeyAfterDays > 0 &&
+                    props.removePrivateKeyAfterDays < 7) ||
+                props.removePrivateKeyAfterDays > 30)
+        ) {
+            scope.node.addError(
+                `Parameter removePrivateKeyAfterDays must be 0 or between 7 and 30. Got ${props.removePrivateKeyAfterDays}`
+            );
         }
 
         const stack = cdk.Stack.of(this).stackName;
@@ -129,7 +132,9 @@ export class KeyPair extends cdk.Construct implements cdk.ITaggable {
                 RemovePrivateKeyAfterDays: props.removePrivateKeyAfterDays || 0,
                 SecretPrefix: props.secretPrefix || 'ec2-private-key/',
                 StackName: stack,
-                Tags: cdk.Lazy.anyValue({ produce: () => this.tags.renderTags() }),
+                Tags: cdk.Lazy.anyValue({
+                    produce: () => this.tags.renderTags(),
+                }),
             },
         });
 
@@ -151,54 +156,58 @@ export class KeyPair extends cdk.Construct implements cdk.ITaggable {
             return existing as lambda.Function;
         }
 
-        const policy = new iam.ManagedPolicy(stack, 'EC2-Key-Pair-Manager-Policy', {
-            managedPolicyName: `${stack.stackName}-${cleanID}`,
-            description: `Used by Lambda ${cleanID}, which is a custom CFN resource, managing EC2 Key Pairs`,
-            statements: [
-                new iam.PolicyStatement({
-                    actions: [
-                        'ec2:CreateKeyPair',
-                        'ec2:DeleteKeyPair',
-                        'ec2:DescribeKeyPairs',
-                        'secretsmanager:ListSecrets',
-                    ],
-                    resources: ['*'],
-                }),
-                new iam.PolicyStatement({
-                    actions: [
-                        'secretsmanager:CreateSecret',
-                        'secretsmanager:TagResource',
-                    ],
-                    resources: ['*'],
-                    conditions: {
-                        StringEquals: {
-                            'aws:RequestTag/CreatedBy': ID,
-                        }
-                    },
-                }),
-                new iam.PolicyStatement({
-                    actions: [
-                        'secretsmanager:DeleteResourcePolicy',
-                        'secretsmanager:DeleteSecret',
-                        'secretsmanager:DescribeSecret',
-                        'secretsmanager:GetResourcePolicy',
-                        'secretsmanager:ListSecretVersionIds',
-                        'secretsmanager:PutResourcePolicy',
-                        'secretsmanager:PutSecretValue',
-                        'secretsmanager:RestoreSecret',
-                        'secretsmanager:UntagResource',
-                        'secretsmanager:UpdateSecret',
-                        'secretsmanager:UpdateSecretVersionStage',
-                    ],
-                    resources: ['*'],
-                    conditions: {
-                        StringEquals: {
-                            'aws:ResourceTag/CreatedBy': ID,
-                        }
-                    },
-                }),
-            ],
-        });
+        const policy = new iam.ManagedPolicy(
+            stack,
+            'EC2-Key-Pair-Manager-Policy',
+            {
+                managedPolicyName: `${stack.stackName}-${cleanID}`,
+                description: `Used by Lambda ${cleanID}, which is a custom CFN resource, managing EC2 Key Pairs`,
+                statements: [
+                    new iam.PolicyStatement({
+                        actions: [
+                            'ec2:CreateKeyPair',
+                            'ec2:DeleteKeyPair',
+                            'ec2:DescribeKeyPairs',
+                            'secretsmanager:ListSecrets',
+                        ],
+                        resources: ['*'],
+                    }),
+                    new iam.PolicyStatement({
+                        actions: [
+                            'secretsmanager:CreateSecret',
+                            'secretsmanager:TagResource',
+                        ],
+                        resources: ['*'],
+                        conditions: {
+                            StringEquals: {
+                                'aws:RequestTag/CreatedBy': ID,
+                            },
+                        },
+                    }),
+                    new iam.PolicyStatement({
+                        actions: [
+                            'secretsmanager:DeleteResourcePolicy',
+                            'secretsmanager:DeleteSecret',
+                            'secretsmanager:DescribeSecret',
+                            'secretsmanager:GetResourcePolicy',
+                            'secretsmanager:ListSecretVersionIds',
+                            'secretsmanager:PutResourcePolicy',
+                            'secretsmanager:PutSecretValue',
+                            'secretsmanager:RestoreSecret',
+                            'secretsmanager:UntagResource',
+                            'secretsmanager:UpdateSecret',
+                            'secretsmanager:UpdateSecretVersionStage',
+                        ],
+                        resources: ['*'],
+                        conditions: {
+                            StringEquals: {
+                                'aws:ResourceTag/CreatedBy': ID,
+                            },
+                        },
+                    }),
+                ],
+            }
+        );
 
         const role = new iam.Role(stack, 'EC2-Key-Pair-Manager-Role', {
             roleName: `${stack.stackName}-${cleanID}`,
@@ -206,8 +215,10 @@ export class KeyPair extends cdk.Construct implements cdk.ITaggable {
             assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
             managedPolicies: [
                 policy,
-                iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
-            ]
+                iam.ManagedPolicy.fromAwsManagedPolicyName(
+                    'service-role/AWSLambdaBasicExecutionRole'
+                ),
+            ],
         });
 
         const fn = new lambda.Function(stack, constructName, {
@@ -216,16 +227,18 @@ export class KeyPair extends cdk.Construct implements cdk.ITaggable {
             description: 'Custom CFN resource: Manage EC2 Key Pairs',
             runtime: lambda.Runtime.NODEJS_10_X,
             handler: 'index.handler',
-            code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/code.zip')),
-            timeout: cdk.Duration.minutes(lambdaTimeout)
+            code: lambda.Code.fromAsset(
+                path.join(__dirname, '../lambda/code.zip')
+            ),
+            timeout: cdk.Duration.minutes(lambdaTimeout),
         });
 
         return fn;
     }
 
     /**
-    * Grants read access to the private key in AWS Secrets Manager
-    */
+     * Grants read access to the private key in AWS Secrets Manager
+     */
     grantRead(grantee: iam.IGrantable) {
         const result = iam.Grant.addToPrincipal({
             grantee,
@@ -236,7 +249,7 @@ export class KeyPair extends cdk.Construct implements cdk.ITaggable {
                 'secretsmanager:ListSecretVersionIds',
             ],
             resourceArns: [this.arn],
-            scope: this
+            scope: this,
         });
         return result;
     }
