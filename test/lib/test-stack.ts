a10443c3291965f001cc5660ce15bb152462b307
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2021-01-19 12:31:22 +0100

    adds stack wide tag for testing

--- test/lib/test-stack.ts
+++ test/lib/test-stack.ts
@@ -6,6 +6,8 @@ export class TestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    cdk.Tags.of(scope).add('Hello', 'Wrold');
+
     new KeyPair(this, 'Test-Key-Pair', {
       name: 'test-key-pair',
       description: 'A test Key Pair',
