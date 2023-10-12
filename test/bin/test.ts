ccaf227c93bb513c417d420cc4e1cfbcb8613a11
udondan <deemes79@googlemail.com>
2023-10-12 18:00:18 +0200

    test: grant current user to read secrets

--- test/bin/test.ts
+++ test/bin/test.ts
@@ -5,25 +5,20 @@ import cdk = require('aws-cdk-lib');
 import { TestStack } from '../lib/test-stack';
 
 async function getIdentity() {
-  try {
-    const stsClient = new STSClient({});
-    const callerIdentity = await stsClient.send(
-      new GetCallerIdentityCommand({})
-    );
-    console.log(callerIdentity);
-  } catch (error) {
-    console.error('Error retrieving identity:', error);
-  }
+  const stsClient = new STSClient({});
+  const callerIdentity = await stsClient.send(new GetCallerIdentityCommand({}));
+  return callerIdentity.Arn?.split('/')[1] as string;
 }
 
 async function main() {
-  await getIdentity();
+  const userName = await getIdentity();
   const app = new cdk.App();
   new TestStack(app, 'EC2KeyPair', {
     env: {
       account: process.env.CDK_DEFAULT_ACCOUNT,
       region: process.env.CDK_DEFAULT_REGION,
     },
+    currentUserName: userName,
   });
 }
 
