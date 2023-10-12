993121560fe726eb5784e066b78fee1e9f5d57de
udondan <deemes79@googlemail.com>
2023-10-12 17:49:48 +0200

    test: get current identity

--- test/bin/test.ts
+++ test/bin/test.ts
@@ -1,12 +1,30 @@
 #!/usr/bin/env node
+import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts';
 import cdk = require('aws-cdk-lib');
 
 import { TestStack } from '../lib/test-stack';
 
-const app = new cdk.App();
-new TestStack(app, 'EC2KeyPair', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+async function getIdentity() {
+  try {
+    const stsClient = new STSClient({});
+    const callerIdentity = await stsClient.send(
+      new GetCallerIdentityCommand({})
+    );
+    console.log(callerIdentity);
+  } catch (error) {
+    console.error('Error retrieving identity:', error);
+  }
+}
+
+async function main() {
+  await getIdentity();
+  const app = new cdk.App();
+  new TestStack(app, 'EC2KeyPair', {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+  });
+}
+
+main();
